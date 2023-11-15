298cf522f440828b77ce1be5902676d4558e3fe2
Abdullah Karagöz <abdullah.karagoz@outlook.com>
2023-11-15 13:38:10 +0300

    Add project files.

---
+++ RedisApp.API/Model/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RedisApp.API.Model
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Seed
+            modelBuilder.Entity<Product>().HasData(
+                new Product() { Id = 1, Name = "Kalem1", Price = 100 },
+                new Product() { Id = 2, Name = "Kalem2", Price = 200 },
+                new Product() { Id = 3, Name = "Kalem3", Price = 300 }
+                );
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
